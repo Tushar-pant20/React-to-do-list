d110db7bb94d10fb4ad5ff3e859d9d7f4d7537ed
tushar-pant20 <tusharpant2001@gmail.com>
2023-09-07 11:00:35 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
 
 function App() {
+
+  const inputfield = document.getElementById('input');
+
+  const [inputList,setInputList] = useState('');
+
+  const [items,setItems] = useState([]);
+
+  const update = (e) => {
+    setInputList(e.target.value);
+  }
+
+  const addItem = () => {
+    setItems((oldItems) => {
+      return [...oldItems, inputList];
+    })
+    setInputList('');
+    inputfield.focus();
+  }
+
+  const deleteItem = (id) => {
+    
+    setItems((oldItems) => {
+      return oldItems.filter((arrayElem,index) => {
+        return index !== id;
+      })
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main_div">
+      <div className="center_div">
+        <br/> 
+        <h1>To-Do List</h1>
+        <br/>
+        <input type='text' id='input' value={inputList} onChange={update} autoFocus placeholder='add item'/>
+        <button onClick={addItem}> + </button>
+
+        <ol>
+          {/* <li>{inputList}</li> */}
+          {items.map((itemval, index) => {
+            return <li key={index}> <span onClick={() => deleteItem(index)}>&#x2716;</span> {itemval}</li>
+          })
+          }  
+        </ol>
+      </div>
     </div>
   );
 }
